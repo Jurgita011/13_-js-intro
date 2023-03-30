1937605288d9e1504870ec94b6c3245ea208d94d
Jurgita <jurgita011@gmail.com>
2023-03-30 19:09:14 +0300

    Console.logo()

---
+++ index.js
@@ -0,0 +1,21 @@
+// vienos eilutes komentaras
+
+
+// multi
+// line
+// komentaras
+
+/*
+multi   (-keliu eiluciu komentaras)
+line
+komentaras 
+*/
+
+console.log(42);    // komentaras salia
+console.log(41);
+console.log(40);
+
+// extra
+
+console.log(39);
+
