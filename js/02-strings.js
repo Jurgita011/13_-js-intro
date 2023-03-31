6f200926be19a68cc920b7826b0bc1e1d5aaf28a
Jurgita <jurgita011@gmail.com>
2023-03-31 07:39:41 +0300

    Strings

--- js/02-strings.js
+++ js/02-strings.js
@@ -2,11 +2,135 @@ console.clear();
 
 /*
 STRINGS- simboliu sarasas/(junginys, tekstas (galbut  bet nebutinai )
+
+ Kintamuju galimybes:
+  -const( default)
+  -let ( tik jei yra poreikis keisti reiksme)
+  -var  (nenaudoti ! nebent zinome ka darome)
+
+  String'o iniciavimas:
+  -viengubos kabutes 
+  -dvigubos kabutes
 */
+//kintamuju iniciavimas
+const vardas =' Jurga';
+console.log(vardas);
 
-console.log('labas');
- console.log('rytas');
- console.log('labas rytas');
- console.log("labas rytas");
+const keturi= 4;
+console.log(keturi);
 
+const keturiTekstu='4';
+console.log(keturiTekstu);
+
+const pinigine=0;
+console.log(pinigine);
+
+console.log('-------------------------');
+//STRINGS
+const firstname='Maryte'
+console.log(firstname);
+
+const lastname='Marytaite'
+console.log(lastname);
+
+// Labas rytas, Maryte!
+console.log('Labas rytas,', firstname,'!');//netinkamas sprendimas
+
+const pasisveikinimas='labas rytas,' + firstname + '!';
+console.log(pasisveikinimas);
+
+// Sveiki, mano vardas Jurga ir man 88 metai!
+const name1 = 'Jurga';
+const age1 = 88;
+
+const prisistatymas ='Sveiki, mano vardas ' + name1 + ' ir man ' + age1 +' metai!' ;
+console.log(prisistatymas);
+
+
+const word1 = 'vasara'
+const perPuse =word1 / 2;
+console.log(perPuse);
+  
+console.clear();
+
+const x ='4'; //'4'-> 4  // '4a'-> NAN
+const y = 5;
+
+console.log(y+x);
+console.log(x-y);
+console.log(x/y);
+console.log(y*x);
+
+console.log(4+ 4);
+console.log('4'+ 4);
+console.log (4+ '4');
+console.log('4'+ '4');
+
+console.log(4 - 4);
+console.log('4'- 4);
+console.log (4 -'4');
+console.log('4'- '4');
+
+console.log(4 / 4);
+console.log('4'/ 4);
+console.log (4 /'4');
+console.log('4'/ '4');
+
+console.log(4 * 4);
+console.log('4'* 4);
+console.log (4 *'4');
+console.log('4' *'4');
+
+ console.clear();
+
+//labas 
+const s1 = 'Labas';
+console.log(s1);
+
+const s2 = 'L' + 'a' + 'b' + 'a' + 's';
+console.log(s2);
+
+const s3 = 'Labas'+ ' ' + 'rytas';
+console.log(s3);
+
+//skaiciai : teigiami ir neigiami
+// variables: constant variables ir variables 
+
+console.clear();
+
+     //2 uzduotis : veiksmai su kintamaisias 
+
+     const a = 14;
+     const b = 5;
+     const c = 3;
+     console.log(a+b+c);
+
+     // Su gimtadieniu, Arunai!
+     const name ='Arunai';
+     const sveikinimas ='Su gimtadieniu , ' + name + '!';
+     console.log(sveikinimas);
+
+     // 1 uzduotis: Kintamuju inicijavimas
+
+     const d = 10;
+     const e = 13;
+     const f = 5;
+     console.log(d);
+     console.log(e);
+     console.log(f);
+    
+   const a1 = 'sveikinu';
+   const a2 = 'su';
+   const a3 = 'Jurginemis';
+   
+   console.log(a1);
+   console.log(a2);
+   console.log(a3);
+
+
+
+  
+    
+    
+    
  
