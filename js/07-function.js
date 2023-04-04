b9c93136851ac69d67d42c563be130bf75816521
Jurgita <jurgita011@gmail.com>
2023-04-04 20:11:16 +0300

    02-Namu darbas

--- js/07-function.js
+++ js/07-function.js
@@ -76,4 +76,33 @@ console.log(hello());
 console.log(hello('Petras'));
 console.log(hello('Maryte', 'Jonas'));
 console.log(hello('Maryte', 'Jonas', 'Ona'));
+ 
 
+
+      //Namu darbai
+
+      function tusciaFunkcija(){
+        return 'false';
+        
+    }
+    
+    console.log(tusciaFunkcija());
+    
+    // DAUGYBA
+    const skaicius1 = 20;
+    const skaicius2 = 3.14;
+    const skaicius3 = -2;
+    
+    function daugyba(sk1, sk2 ){
+        const sandauga = sk1 * sk2;
+        return sandauga;
+    }
+    
+    console.log(daugyba(skaicius1, skaicius2));
+    console.log(daugyba(skaicius3, skaicius2));
+    console.log(daugyba(skaicius1, skaicius3));
+    
+    //skaitmenu kiekis skaiciuje  
+   
+   
+   
